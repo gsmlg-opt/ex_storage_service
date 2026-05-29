0d3332c1c90f758c10f6669feb58c774f69bdc0d
Jonathan Gao <gsmlg.com@gmail.com>
2026-05-29 10:29:43 +0800

    fix: drop config_path from CLI mix.exs for standalone hex install

--- apps/ex_storage_service_cli/mix.exs
+++ apps/ex_storage_service_cli/mix.exs
@@ -8,6 +8,9 @@ defmodule ExStorageServiceCli.MixProject do
     [
       app: :ex_storage_service_cli,
       version: @version,
+      build_path: "../../_build",
+      deps_path: "../../deps",
+      lockfile: "../../mix.lock",
       elixir: "~> 1.19",
       start_permanent: false,
       deps: deps(),
@@ -15,7 +18,7 @@ defmodule ExStorageServiceCli.MixProject do
       package: package(),
       description: "CLI tool for ExStorageService S3-compatible object storage",
       source_url: @source_url
-    ] ++ umbrella_paths()
+    ]
   end
 
   def application do
@@ -47,20 +50,6 @@ defmodule ExStorageServiceCli.MixProject do
       files: ~w(lib mix.exs README.md LICENSE)
     ]
   end
-
-  # Only set umbrella paths when running inside the umbrella project.
-  # When installed standalone from hex.pm, these paths don't exist.
-  defp umbrella_paths do
-    if File.exists?(Path.expand("../../mix.exs", __DIR__)) do
-      [
-        build_path: "../../_build",
-        config_path: "../../config/config.exs",
-        deps_path: "../../deps",
-        lockfile: "../../mix.lock"
-      ]
-    else
-      []
-    end
-  end
 end
+
 
