bedaf9b3fd60bb238829ba87bdbb16810c56e00c
Jonathan Gao <gsmlg.com@gmail.com>
2026-05-29 13:09:50 +0800

    fix: lower elixir requirement to ~> 1.18 across all apps

--- apps/ex_storage_service/mix.exs
+++ apps/ex_storage_service/mix.exs
@@ -9,7 +9,7 @@ defmodule ExStorageService.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       deps: deps()
--- apps/ex_storage_service_web/mix.exs
+++ apps/ex_storage_service_web/mix.exs
@@ -9,7 +9,7 @@ defmodule ExStorageServiceWeb.MixProject do
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
       lockfile: "../../mix.lock",
-      elixir: "~> 1.19",
+      elixir: "~> 1.18",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       listeners: [Phoenix.CodeReloader],
